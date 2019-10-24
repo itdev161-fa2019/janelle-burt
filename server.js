16772d4b280baf3ce9dc092eb2b5391a024e63b4
Your Name <you@example.com>
2019-10-24 01:23:30 -0500

    Staged

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ import connectDatabase from './config/db';
 const app = express();
 
 connectDatabase();
-
 app.use(express.json({extended:false}));
-
+/*
 @route 
 @desc 
+*/
 
 app.get('/', (req, res) =>
 res.send('http get request sent to root api endpoint')
 );
-
+/*
 @route 
 @desc
-
+*/
 app.post('/api/users', (req,res) => {
     console.log(req.body);
     res.send(req.body);
-} );
+});
 
-app.listen(3000, () => console.log('Express server running on Port 3000'));
+app.listen(3000, () => console.log(`Express server running on Port 3000`));
